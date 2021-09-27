7ad5e74a17a78ae8e51c0f31370d334b18bfa94c
Pedro Hoppe <pedrohbh@github.com>
2021-09-27 20:22:34 -0300

    Fim da página 174

---
+++ src/componentes/LvRegistro.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>Registre-se no sistema</h2>
+  <hr />
+
+  <lv-input titulo="Nome">
+    <input type="text" v-model="usuario.nome" />
+  </lv-input>
+  <lv-input titulo="Email">
+    <input type="email" v-model="usuario.email" />
+  </lv-input>
+  <lv-input titulo="Senha">
+    <input type="password" v-model="usuario.senha" />
+  </lv-input>
+  <br />
+  <input
+    @click="registrar"
+    type="submit"
+    :value="estado"
+    :disabled="estado != 'Registrar'"
+  />
+</template>
+
+<script>
+import Usuario from "./../servicos/usuario";
+export default {
+  name: "lv-registro",
+  data() {
+    return { estado: "Registrar", usuario: { nome: "", email: "", senha: "" } };
+  },
+  methods: {
+    registrar() {
+      this.estado = "Carregando...";
+      Usuario.registrar(this.usuario)
+        .then((resposta) => {
+          if (resposta.data.sucesso) {
+            alert("Registrado com sucesso");
+            this.$router.replace("/");
+          } else this.estado = "Registrar";
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
+</script>
