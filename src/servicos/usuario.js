6d4e0f8a18b125b92aab90867b9adb339acefb00
Pedro Hoppe <pedrohbh@github.com>
2021-09-30 16:29:31 -0300

    Configurando cap 16 - parte 1

--- src/servicos/usuario.js
+++ src/servicos/usuario.js
@@ -4,4 +4,7 @@ export default {
     registrar: ({ nome, email, senha}) => {
         return http.post('usuario', { nome, email, senha });
     },
+    logar: ({email, senha}) => {
+        return http.post('usuario/login', {email, senha});
+    },
 }
