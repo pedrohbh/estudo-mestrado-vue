2210f3821ebc8d36b0f5c3b4aed77f0b369cfff3
Pedro Hoppe <pedrohbh@github.com>
2021-09-30 18:53:02 -0300

    Fim da página 182 - BETA

---
+++ test/Painel.test.js
@@ -0,0 +1,37 @@
+import { mount } from '@vue/test-utils'
+import LvPainel from './../src/componentes/LvPainel.vue'
+import router from '../src/router'
+import mockVuex from './mockvuex'
+
+test('verificando nome no painel', async () => {
+    const wrapper = mount(LvPainel, {
+        global: {
+            plugins: [router, mockVuex],
+            mocks: {
+                emitter: { on: () => { }, emit: () => { } }
+            },
+        }
+    })
+    await router.isReady()
+    const span = wrapper.find('span')
+    expect(span.text()).toEqual('Painel logado com Leonardo')
+})
+
+test('saindo do sistema com mock, verificando se botão está lá',
+    async () => {
+        const wrapper = mount(LvPainel, {
+            global: {
+                plugins: [router, mockVuex],
+                mocks: {
+                    emitter: { on: () => { }, emit: () => { } }
+                },
+            }
+        })
+        let acao = ''
+        wrapper.vm.sair = function () {
+            acao = 'saiu'
+        }
+        const botao = wrapper.find('button')
+        await botao.trigger('click')
+        expect(acao).toEqual('saiu')
+    })
---
+++ test/mockvue.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    state: {
+        usuario: {
+            id: 1,
+            nome: 'Leonardo',
+            email: 'leonardo@email.com'
+        }
+    },
+})
