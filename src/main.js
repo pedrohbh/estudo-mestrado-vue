8769255c7dffc57a1306b90d768df19db4b507bf
Pedro Hoppe <pedrohbh@github.com>
2021-10-04 17:13:31 -0300

    Fim da página 190 - BETA

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
+import mitt from 'mitt'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
 import LvInput from './componentes/LvInput.vue'
 
-createApp(App).use(store).use(router).use(store).component('lv-input', LvInput).
-mount('#app')
+const emitter = mitt()
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.component('lv-input', LvInput)
+app.config.globalProperties.emitter = emitter
+app.mount('#app')
