adf277a47a0848ff287920e9ff11b8c45e9fc3a5
Pedro Hoppe <pedrohbh@github.com>
2021-10-04 19:49:23 -0300

    Fim do capítulo 17

--- src/componentes/LvPainel.vue
+++ src/componentes/LvPainel.vue
@@ -1,11 +1,17 @@
 <template>
   <span>Painel logado com {{ $store.state.usuario.nome }}</span>
   <button @click="sair">Sair</button>
+  <hr />
+  <lv-nova-nota></lv-nova-nota>
 </template>
 
 <script>
+import LvNovaNota from "./LvNovaNota.vue";
 export default {
   name: "lv-painel",
+  components: {
+    LvNovaNota,
+  },
   methods: {
     sair() {
       this.$store.dispatch("logarUsuario", { id: 0, nome: "", email: "" });
