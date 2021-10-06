d7bf7bbe6a59e7529057b41f14d81d662cf0e65c
Pedro Hoppe <pedrohbh@github.com>
2021-10-06 17:37:21 -0300

    Fim da págna 196

--- src/componentes/LvListaNotas.vue
+++ src/componentes/LvListaNotas.vue
@@ -5,3 +5,33 @@
   </ul>
 </template>
 
+<script>
+import Nota from "./../servicos/notas";
+import LvItemNota from "./LvItemNota.vue";
+
+export default {
+  name: "lv-lista-nota",
+  components: { LvItemNota },
+  data() {
+    return { notas: {} };
+  },
+  mounted() {
+    this.emitter.on("atualizacao", this.atualizar);
+    this.atualizar();
+  },
+  methods: {
+    atualizar() {
+      Nota.listar(this.campos)
+        .then((resposta) => {
+          if (resposta.data.sucesso) this.notas = resposta.data.notas;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+  computed: {
+    campos() {
+      return this.$store.state.usuario.id;
+    },
+  },
+};
+</script>
