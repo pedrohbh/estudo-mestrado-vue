37b161de09204cfcd93d1c6bcf651aeb4cf2684b
Pedro Hoppe <pedrohbh@github.com>
2021-10-06 17:20:13 -0300

    Fim da página 193

--- src/servicos/notas.js
+++ src/servicos/notas.js
@@ -4,4 +4,7 @@ export default {
     criar: ({ conteudo, usuario_id }) => {
         return http.post('notas', { conteudo, usuario_id });
     },
+    listar: (usuario_id) => {
+        return http.get('notas?usuario_id=' + usuario_id);
+    },
 }
---
+++ test/ItemNota.test.js
@@ -0,0 +1,17 @@
+import { mount } from '@vue/test-utils'
+import LvItemNota from './../src/componentes/LvItemNota.vue'
+import router from '../src/router'
+
+test('iniciando item da lista', async () => {
+    const wrapper = mount(LvItemNota, {
+        props: {
+            nota: { conteudo: 'academia', id: 4 }
+        },
+        global: {
+            plugins: [router]
+        }
+    })
+    await router.isReady()
+    const titulo = wrapper.find('span')
+    expect(titulo.text()).toEqual('academia')
+})
